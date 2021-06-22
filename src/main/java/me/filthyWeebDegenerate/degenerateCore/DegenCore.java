97296d70309ef94a243abaec2a52a20386ce1d77
FilthyWeebDegenerate <83183804+BathtubInvis@users.noreply.github.com>
2021-06-22 17:48:00 -0400

    Initial commit

---
+++ src/main/java/me/filthyWeebDegenerate/degenerateCore/DegenCore.java
@@ -0,0 +1,40 @@
+package me.filthyWeebDegenerate.degenerateCore;
+
+import me.filthyWeebDegenerate.degenerateCore.API.ItemRegistry;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(DegenCore.ID)
+public class DegenCore {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String ID = "degenerate_core";
+
+    public DegenCore() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        bus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {}
+
+    private static class ItemReg extends ItemRegistry {
+        @Override
+        public void register(IEventBus bus) {
+            registerRegistry(DegenCore.ID);
+
+            registerCustomizedItem("e", () -> new BlockItem(Blocks.CHAIN, new Item.Properties()));
+            super.register(bus);
+        }
+    }
+}
