4a7f8fe8898be1ff2d96999835ec97248a2988f4
FilthyWeebDegenerate <83183804+BathtubInvis@users.noreply.github.com>
2021-06-23 17:42:35 -0400

    Added an ArmorMaterialConstructor

---
+++ src/main/java/me/filthyWeebDegenerate/degenerateCore/API/armor/ArmorMaterialConstructor.java
@@ -0,0 +1,74 @@
+package me.filthyWeebDegenerate.degenerateCore.API.armor;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.common.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum ArmorMaterialConstructor implements IArmorMaterial {
+    ;
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Lazy<Ingredient> repairMaterialLazy;
+
+    ArmorMaterialConstructor(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, float knockbackResistance, Supplier<Ingredient> repairMaterialSupplier) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairMaterialLazy = Lazy.concurrentOf(repairMaterialSupplier);
+    }
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return 0;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}
